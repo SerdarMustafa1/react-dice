98ad9bc0a39f85cec44c247425e8c370b4829867
Serdar Mustafa <serdarmustafa@SerdarPsonalMBP.lan>
2019-07-21 10:19:25 +0300

    Updates

---
+++ precache-manifest.8a5ae81bf6d2cc17a2c42a83704b0954.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4071905f754529b980910a1aa4866822",
+    "url": "/SerdarMustafa1/react-dice.git/index.html"
+  },
+  {
+    "revision": "10b631aec053a20fddf6",
+    "url": "/SerdarMustafa1/react-dice.git/static/css/main.0f253e56.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/SerdarMustafa1/react-dice.git/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "10b631aec053a20fddf6",
+    "url": "/SerdarMustafa1/react-dice.git/static/js/main.55a40b28.chunk.js"
+  },
+  {
+    "revision": "8916ba0325a885735e73",
+    "url": "/SerdarMustafa1/react-dice.git/static/js/runtime~main.e8894d75.js"
+  }
+]);
